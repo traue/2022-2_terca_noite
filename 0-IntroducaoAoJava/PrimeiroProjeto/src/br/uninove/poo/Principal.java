8a9d0ff00dbd24456845b009d3b5d4a9c3e4aa58
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-16 21:59:19 -0300

    Criação do projeto de revisão

---
+++ 0-IntroducaoAoJava/PrimeiroProjeto/src/br/uninove/poo/Principal.java
@@ -0,0 +1,17 @@
+package br.uninove.poo;
+
+public class Principal {
+    
+    //isso é um comentário de "linha"
+    
+    /*
+    * Isso é um bloco de comentário 
+    * Posso pular linhas livremente
+    * :D
+    */
+    
+    public static void main(String[] args) { 
+        System.out.print("Essa turma é top!");
+        System.out.print("\nVou dar 10 para todo mundo!");
+    }
+}
