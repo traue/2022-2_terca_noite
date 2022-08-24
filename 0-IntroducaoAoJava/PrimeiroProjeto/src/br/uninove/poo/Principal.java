3a8e3b192ac2702cc5a83c83ee38fa02f6384b8a
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-23 21:23:57 -0300

    Continuação

--- 0-IntroducaoAoJava/PrimeiroProjeto/src/br/uninove/poo/Principal.java
+++ 0-IntroducaoAoJava/PrimeiroProjeto/src/br/uninove/poo/Principal.java
@@ -1,5 +1,7 @@
 package br.uninove.poo;
 
+import java.util.Scanner;
+
 public class Principal {
     
     //isso é um comentário de "linha"
@@ -11,7 +13,46 @@ public class Principal {
     */
     
     public static void main(String[] args) { 
-        System.out.print("Essa turma é top!");
-        System.out.print("\nVou dar 10 para todo mundo!");
+        //Imprimindo no console:
+        System.out.println("Essa turma é top!");
+        System.out.println("Vou dar 10 para todo mundo!");
+        
+        //variáveis
+        char sexo = 'X';
+        String nome = "Thiago G. Traue"; //armazenar textos
+        byte idade = 35; //para armazenar números inteiros 
+        float altura = 1.83f;
+        boolean fumante = false; //variáveis booleanas (V/F - True/False)
+        
+        //impressão dos dados com concatenação:
+        System.out.println("Olá " + nome + ", tudo bem?");
+        System.out.println("Ah! Legal, você tem " + idade + " anos!");
+        System.out.println("Nossa, você tem " + altura + "m Legal!");
+        
+        //-------------------------------
+        
+        //quais operações matemáticas podemos fazer com Java?
+        //todas!
+        
+        float x = 30;
+        float y = 21;
+        float soma = x + y;
+        float subtracao = x - y;
+        float multiplicacao = x * y;
+        float divisao = x / y;
+        
+        System.out.println("A soma de " + x + " + " + y + " = " + soma);
+        System.out.println("A subtração de " + x + " - " + y + " = " + subtracao);
+        System.out.println("A multiplcação de " + x + " x " + y + " = " + multiplicacao);
+        System.out.println("A divisão de " + x + " / " + y + " = " + divisao);
+        
+        int a = 27;
+        int b = 4;
+        int resto = 27 % 4;
+        System.out.println("O resto é " + resto);
+        
+        //lendo valores do teclado:
+        Scanner sc = new Scanner(System.in);
+        
     }
 }
