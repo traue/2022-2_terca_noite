a479217d00dd725345e546bc33a9b7e78dad3e8a
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-11-01 20:47:29 -0300

    Finalização do projeto

--- Primeiro Projeto/Primeiro Projeto/Form1.Designer.cs
+++ Primeiro Projeto/Primeiro Projeto/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbNome = new System.Windows.Forms.Label();
             this.lbSobrenome = new System.Windows.Forms.Label();
             this.txtNome = new System.Windows.Forms.TextBox();
@@ -97,7 +98,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(214)))), ((int)(((byte)(254)))));
-            this.ClientSize = new System.Drawing.Size(240, 305);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(311, 305);
             this.Controls.Add(this.lbResultado);
             this.Controls.Add(this.btBoasVindas);
             this.Controls.Add(this.txtSobrenome);
