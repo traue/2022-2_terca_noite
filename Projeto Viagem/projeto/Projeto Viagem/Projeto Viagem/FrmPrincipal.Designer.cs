2f8e98a8654dfb6fa2cbf96a806d78f0ad42d1be
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-11-01 22:02:20 -0300

    Inicio do projeto

---
+++ Projeto Viagem/projeto/Projeto Viagem/Projeto Viagem/FrmPrincipal.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Projeto_Viagem
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.lbDistancia = new System.Windows.Forms.Label();
+            this.lbConsumo = new System.Windows.Forms.Label();
+            this.lbCombustivel = new System.Windows.Forms.Label();
+            this.lbPedagio = new System.Windows.Forms.Label();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.txtConsumo = new System.Windows.Forms.TextBox();
+            this.txtCombustivel = new System.Windows.Forms.TextBox();
+            this.txtPedagio = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbDistancia
+            // 
+            this.lbDistancia.AutoSize = true;
+            this.lbDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDistancia.Location = new System.Drawing.Point(124, 49);
+            this.lbDistancia.Name = "lbDistancia";
+            this.lbDistancia.Size = new System.Drawing.Size(188, 24);
+            this.lbDistancia.TabIndex = 0;
+            this.lbDistancia.Text = "Distância a percorrer:";
+            // 
+            // lbConsumo
+            // 
+            this.lbConsumo.AutoSize = true;
+            this.lbConsumo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbConsumo.Location = new System.Drawing.Point(65, 104);
+            this.lbConsumo.Name = "lbConsumo";
+            this.lbConsumo.Size = new System.Drawing.Size(247, 24);
+            this.lbConsumo.TabIndex = 1;
+            this.lbConsumo.Text = "Consumo médio do veículo:";
+            // 
+            // lbCombustivel
+            // 
+            this.lbCombustivel.AutoSize = true;
+            this.lbCombustivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCombustivel.Location = new System.Drawing.Point(35, 159);
+            this.lbCombustivel.Name = "lbCombustivel";
+            this.lbCombustivel.Size = new System.Drawing.Size(277, 24);
+            this.lbCombustivel.TabIndex = 2;
+            this.lbCombustivel.Text = "Valor médio do combustível: R$";
+            // 
+            // lbPedagio
+            // 
+            this.lbPedagio.AutoSize = true;
+            this.lbPedagio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPedagio.Location = new System.Drawing.Point(12, 214);
+            this.lbPedagio.Name = "lbPedagio";
+            this.lbPedagio.Size = new System.Drawing.Size(300, 24);
+            this.lbPedagio.TabIndex = 3;
+            this.lbPedagio.Text = "Valor total gasto com pedágios: R$";
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDistancia.Location = new System.Drawing.Point(318, 46);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(87, 29);
+            this.txtDistancia.TabIndex = 4;
+            // 
+            // txtConsumo
+            // 
+            this.txtConsumo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsumo.Location = new System.Drawing.Point(318, 101);
+            this.txtConsumo.Name = "txtConsumo";
+            this.txtConsumo.Size = new System.Drawing.Size(87, 29);
+            this.txtConsumo.TabIndex = 5;
+            // 
+            // txtCombustivel
+            // 
+            this.txtCombustivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCombustivel.Location = new System.Drawing.Point(318, 156);
+            this.txtCombustivel.Name = "txtCombustivel";
+            this.txtCombustivel.Size = new System.Drawing.Size(87, 29);
+            this.txtCombustivel.TabIndex = 6;
+            // 
+            // txtPedagio
+            // 
+            this.txtPedagio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPedagio.Location = new System.Drawing.Point(318, 211);
+            this.txtPedagio.Name = "txtPedagio";
+            this.txtPedagio.Size = new System.Drawing.Size(87, 29);
+            this.txtPedagio.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(16, 266);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(389, 54);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular gasto da viagem";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 378);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPedagio);
+            this.Controls.Add(this.txtCombustivel);
+            this.Controls.Add(this.txtConsumo);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.lbPedagio);
+            this.Controls.Add(this.lbCombustivel);
+            this.Controls.Add(this.lbConsumo);
+            this.Controls.Add(this.lbDistancia);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmPrincipal";
+            this.Text = "Calculadora da Viagem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDistancia;
+        private System.Windows.Forms.Label lbConsumo;
+        private System.Windows.Forms.Label lbCombustivel;
+        private System.Windows.Forms.Label lbPedagio;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.TextBox txtConsumo;
+        private System.Windows.Forms.TextBox txtCombustivel;
+        private System.Windows.Forms.TextBox txtPedagio;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
